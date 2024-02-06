1a489cc87992c7af4bb24fda46ee5f2f582e9936
Nimo08 <nimomohamed800@gmail.com>
2024-02-06 19:41:17 +0300

    Correcting task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,10 @@ export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
   return Promise.all(promises)
     .then(([photoResponse, userResponse]) => {
-      const { body: photoBody } = photoResponse;
+      const { body } = photoResponse;
       const { firstName, lastName } = userResponse;
 
-      console.log(photoBody, firstName, lastName);
+      console.log(body, firstName, lastName);
     })
     .catch((error) => {
       console.log('Signup system offline', error);
