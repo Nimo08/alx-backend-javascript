d742b85ae666c07458eb4852ccaed1f9df546040
Nimo08 <nimomohamed800@gmail.com>
2024-02-07 09:51:26 +0300

    Retrying: task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-  return Promise.all(promises)
-    .then((results) => console.log(`${results[0].body}, ${results[1].firstName}, ${results[1].lastName}`))
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((results) => console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
