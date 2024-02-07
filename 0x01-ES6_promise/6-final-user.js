dc3ca557bea24a84dcabcb06cc3518c3dc43e280
Nimo08 <nimomohamed800@gmail.com>
2024-02-07 12:06:56 +0300

    Retrying task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.value || result.reason,
   })));
 }
