885978f1a0dbb9d61182d98ef8ef034678a54387
Nimo08 <nimomohamed800@gmail.com>
2024-02-07 19:49:44 +0300

    Used 2 promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises);
+  const signup = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([signup, photo])
+    .then((results) => {
+      const finalResults = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          finalResults.push({ status: result.status, value: result.value });
+        } else {
+          finalResults.push({ status: result.status, value: `${result.reason}` });
+        }
+      });
+      return finalResults;
+    });
 }
