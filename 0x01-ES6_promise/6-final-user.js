0e79077d8b7753afec52ac5e96c6ec5a02d4a59c
Nimo08 <nimomohamed800@gmail.com>
2024-02-07 18:51:46 +0300

    Reverted to previous version

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((results) => {
-    console.log(results);
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  }).catch((error) => [{ status: 'rejected', value: error }]);
+  return Promise.allSettled(promises);
 }
