d297f59c26dc013f523581af81d74a1cea4adc3c
Nimo08 <nimomohamed800@gmail.com>
2024-02-08 18:51:05 +0300

    Removing symbols for attributes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,11 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    const clonedCar = new Car(this[_brand], this[_motor], this[_color]);
-    return clonedCar;
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
