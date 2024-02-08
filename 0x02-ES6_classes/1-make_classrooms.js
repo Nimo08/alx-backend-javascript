ea91691876bad82479d86148c670be0c65e56480
Nimo08 <nimomohamed800@gmail.com>
2024-02-08 12:03:22 +0300

    Adding scripts for tasks & installation

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const array = [
+    // new ClassRoom objects in an array
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return array;
+}
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  _name;
+  _length;
+  _students;
+
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      } else {
+        this._name = name;
+      }
+    if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      } else {
+        this._length = length;
+      }
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = newStudents;
+  }
+}
