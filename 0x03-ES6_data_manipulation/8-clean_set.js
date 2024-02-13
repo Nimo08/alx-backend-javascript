853f588dc372cf406d1301157a12be2435093c86
Nimo08 <nimomohamed800@gmail.com>
2024-02-13 12:15:37 +0300

    Retrying task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString) {
-  // arrau to contain portions of set that start with startString
-  const result = [];
-  for (const value of set) {
-    // check if current value starts with startString
-    if (value.startsWith(startString)) {
-      // append the rest of the string
-      result.push(value.substring(startString.length));
-    }
+  // check if startString is undefined or empty
+  if (startString === undefined || startString.length === 0) {
+    return '';
   }
-  // concatenate elements of array into a single string
-  return result.join('-');
+  // convert set into array to make it iterable
+  return [...set]
+    // filter out elements not starting with startString
+    .filter((element) => (element !== undefined ? element.startsWith(startString) : ''))
+  // remove startString from beginning of each element using slice
+    .map((element) => (element !== undefined ? element.slice(startString.length) : ''))
+    .join('-');
 }
