6a1899626683fc03be4d1cb201a9dd7de07574eb
Nimo08 <nimomohamed800@gmail.com>
2024-04-02 12:15:32 +0300

    Modified 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
 // Using Process stdin
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf-8');
-process.stdin.on('data', (input) => {
-  const trimmedInput = input.trim();
-  console.log(`Your name is: ${trimmedInput}`);
+process.stdin.on('data', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
