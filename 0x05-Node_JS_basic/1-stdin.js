01f3af1174221f25a7c1c46c9269528ddcfaa57a
Nimo08 <nimomohamed800@gmail.com>
2024-04-02 12:19:05 +0300

    Use process.stdout.write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
 // Using Process stdin
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', () => {
-  const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
-  }
+process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
