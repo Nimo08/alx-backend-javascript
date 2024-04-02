b7f5f4fc53b68d1c2268987cde4c79d41121dc12
Nimo08 <nimomohamed800@gmail.com>
2024-04-02 12:22:03 +0300

    Test initial 1-stdin.js script

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 // Using Process stdin
-process.stdout.write('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (input) => {
+  const trimmedInput = input.trim();
+  console.log(`Your name is: ${trimmedInput}`);
 });
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  console.log('This important software is now closing');
 });
